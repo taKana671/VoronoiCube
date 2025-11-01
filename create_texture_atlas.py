74efd7f7af74a1a36247edacfbb37476782a0da8
taKana671 <kanae5321@gmail.com>
2025-11-01 14:20:37 +0900

    First commit

---
+++ create_texture_atlas.py
@@ -0,0 +1,137 @@
+from datetime import datetime
+import random
+
+import cv2
+import numpy as np
+
+from noise import VoronoiNoise, VoronoiEdges, VoronoiRoundEdges
+
+
+def output(img, stem, parent='.', with_suffix=True):
+    if with_suffix:
+        now = datetime.now()
+        stem = f'{stem}_{now.strftime("%Y%m%d%H%M%S")}'
+
+    file_path = f"{parent}/{stem}.png"
+    # file_name = f'{stem}_{now.strftime("%Y%m%d%H%M%S")}.{ext}'
+    cv2.imwrite(file_path, img)
+
+
+class Faces:
+
+    def __init__(self, noise, size, grid):
+        self.noise = noise
+        self.size = size
+        self.grid = grid
+
+    def convert_arr(self, arr):
+        arr = arr.reshape(self.size, self.size, 3)
+        return np.clip(arr * 255, a_min=0, a_max=255).astype(np.uint8)
+
+    def create_top_bottom(self, z, t):
+        arr = np.array(
+            [self.noise(x + t, y + t, z + t)
+                for y in np.linspace(0, self.grid, self.size)
+                for x in np.linspace(0, self.grid, self.size)]
+        )
+        arr = self.convert_arr(arr)
+        return arr
+
+    def create_left_right(self, x, t):
+        arr = np.array(
+            [self.noise(x + t, y + t, z + t)
+                for z in np.linspace(0, self.grid, self.size)
+                for y in np.linspace(0, self.grid, self.size)]
+        )
+        arr = self.convert_arr(arr)
+        return arr
+
+    def create_forward_back(self, y, t):
+        arr = np.array(
+            [self.noise(x + t, y + t, z + t)
+                for z in np.linspace(0, self.grid, self.size)
+                for x in np.linspace(0, self.grid, self.size)]
+        )
+        arr = self.convert_arr(arr)
+        return arr
+
+    def output(self, img):
+        name = self.__class__.__name__.lower()
+        output(img, name)
+
+
+class Bottom(Faces):
+
+    def generate_image(self, t=0):
+        img = self.create_top_bottom(z=self.grid, t=t)
+        return img
+
+
+class Top(Faces):
+
+    def generate_image(self, t=0):
+        img = self.create_top_bottom(z=0, t=t)
+        img = img[:, ::-1]
+        img = np.rot90(img, 2)
+        return img
+
+
+class Back(Faces):
+
+    def generate_image(self, t=0):
+        img = self.create_forward_back(y=0, t=t)
+        return img
+
+
+class Forward(Faces):
+
+    def generate_image(self, t=0):
+        img = self.create_forward_back(y=self.grid, t=t)
+        img = img[:, ::-1]
+        return img
+
+
+class Left(Faces):
+
+    def generate_image(self, t=0):
+        img = self.create_left_right(x=0, t=t)
+        img = img[:, ::-1]
+        return img
+
+
+class Right(Faces):
+
+    def generate_image(self, t=0):
+        img = self.create_left_right(x=self.grid, t=t)
+        return img
+
+
+class TextureAtlasGenerator:
+
+    def __init__(self, noise, grid=4, size=256):
+        self.noise = noise
+        self.grid = grid
+        self.size = size
+
+    def generate_texture(self, t=0):
+        faces = [Back, Forward, Left, Right, Bottom, Top]
+        bg_img = np.zeros((self.size * 2, self.size * 4, 3), dtype=np.uint8)
+        w = h = 0
+
+        for i, face in enumerate(faces):
+            face_creator = face(self.noise, self.size, self.grid)
+            img = face_creator.generate_image(t)
+            # face_creator.output(img)
+
+            h = i // 4 * self.size
+            w = i % 4 * self.size
+            bg_img[h: h + self.size, w: w + self.size] = img
+
+        output(bg_img, 'atras')
+        return bg_img
+
+
+if __name__ == '__main__':
+    tex_atlas = TextureAtlasGenerator()
+    t = random.uniform(0, 1000)
+    tex_atlas.generate_texture(t)
---
+++ voronoi_cube.py
@@ -0,0 +1,237 @@
+from datetime import datetime
+import itertools
+import random
+
+import cv2
+import numpy as np
+from scipy.spatial import Voronoi, voronoi_plot_2d
+import sys
+
+from panda3d.core import Point3, LColor, NodePath, Vec3, Vec2
+from panda3d.core import TextureStage, TexGenAttrib
+from direct.showbase.ShowBase import ShowBase
+from direct.showbase.ShowBaseGlobal import globalClock
+from direct.showbase.InputStateGlobal import inputState
+from panda3d.core import AntialiasAttrib, Texture
+import numpy as np
+
+from shapes import Sphere, Box
+from noise import VoronoiNoise, VoronoiEdges, VoronoiRoundEdges
+from create_texture_atlas import TextureAtlasGenerator
+from panda3d.core import Texture, TextureStage
+
+
+class VoronoiCube(ShowBase):
+
+    def __init__(self):
+        super().__init__()
+        self.disable_mouse()
+        self.render.set_antialias(AntialiasAttrib.MAuto)
+
+        self.camera_root = NodePath('camera_root')
+        self.camera_root.reparent_to(self.render)
+
+        self.camera.set_pos(0, -100, 50)
+        # self.camera.set_pos(0, 100, 50)
+        self.camera.look_at(Point3(0, 0, 0))
+        self.camera.reparent_to(self.camera_root)
+
+        self.dragging = False
+        self.before_mouse_pos = None
+        self.show_wireframe = False
+
+        self.accept('d', self.toggle_wireframe)
+        self.accept('mouse1', self.mouse_click)
+        self.accept('mouse1-up', self.mouse_release)
+        self.accept('escape', sys.exit)
+        self.task_mgr.add(self.update, 'update')
+
+        self.create_box()
+        # self.create_vertices()
+
+    def toggle_wireframe(self):
+        if self.show_wireframe:
+            self.box.set_render_mode_filled()
+        else:
+            self.box.set_render_mode_wireframe()
+
+        # self.toggle_wireframe()
+        self.show_wireframe = not self.show_wireframe
+
+    def change_uv(self, segs):
+        points = (segs + 1) * (segs + 1)
+
+        # bottom
+        u_end, v_end = 1 / 4, 1 / 2
+
+        for i in range(points):
+            u = u_end - (segs - i % (segs + 1)) * (0.25 / segs)
+            v = v_end - i // (segs + 1) * (0.5 / segs)
+            yield (u, v)
+
+        # top
+        u_end, v_end = 1 / 2, 1 / 2
+
+        for i in range(points):
+            u = u_end - (segs - i % (segs + 1)) * (0.25 / segs)
+            v = v_end - (segs - i // (segs + 1)) * (0.5 / segs)
+            yield (u, v)
+
+        # back
+        u_end, v_end = 1 / 4, 1
+
+        for i in range(points):
+            u = u_end - (segs - i // (segs + 1)) * (0.25 / segs)
+            v = v_end - (segs - i % (segs + 1)) * (0.5 / segs)
+            yield (u, v)
+
+        # front
+        u_end, v_end = 0.5, 1
+
+        for i in range(points):
+            u = u_end - i // (segs + 1) * (0.25 / segs)
+            v = v_end - (segs - i % (segs + 1)) * (0.5 / segs)
+            yield (u, v)
+
+        # left
+        u_end, v_end = 0.75, 1
+
+        for i in range(points):
+            u = u_end - i % (segs + 1) * (0.25 / segs)
+            v = v_end - (segs - i // (segs + 1)) * (0.5 / segs)
+            yield (u, v)
+
+        # right
+        u_end, v_end = 1.0, 1.0
+
+        for i in range(points):
+            u = u_end - (segs - i % (segs + 1)) * (0.25 / segs)
+            v = v_end - (segs - i // (segs + 1)) * (0.5 / segs)
+            yield (u, v)
+
+    def create_box(self):
+        segs = 4
+        stride = 12
+        box = Box(width=20, depth=20, height=20, segs_d=segs, segs_w=segs, segs_z=segs).create()
+        box.set_pos_hpr(Point3(0, 0, 0), Vec3(0, 0, 0))
+        box.reparent_to(self.render)
+        # box.set_scale(0.1)
+
+        geom_node = box.node()
+        geom = geom_node.modify_geom(0)
+        vdata = geom.modify_vertex_data()
+        vdata_arr = vdata.modify_array(0)
+        vdata_mem = memoryview(vdata_arr).cast('B').cast('f')
+
+        for i, (u, v) in enumerate(self.change_uv(segs)):
+            idx = i * stride
+            vdata_mem[idx + 10] = u
+            vdata_mem[idx + 11] = v
+            # print(f"u: {u}, v: {v}")
+       
+        tex = self.loader.load_texture('atras.png')
+        # voronoi = VoronoiEdges()
+        # image_generator = TextureAtlasGenerator(voronoi.vmix3)
+        # t = random.uniform(0, 1000)
+        # img = image_generator.generate_texture(t)
+        # img = cv2.rotate(img, cv2.ROTATE_180)
+        # img = cv2.flip(img, 1)
+
+        # tex = Texture('image')
+        # tex.setup_2d_texture(
+        #     256 * 4,
+        #     256 * 2,
+        #     Texture.T_unsigned_byte,
+        #     Texture.F_rgb
+        # )
+
+        # tex.set_ram_image(img)
+
+        # read documents！
+        tex.setWrapU(Texture.WM_clamp)
+        tex.setWrapV(Texture.WM_clamp)
+        tex.setMagfilter(Texture.FTNearest)
+        tex.setMinfilter(Texture.FTNearest)
+
+        box.set_texture(tex)
+        box.set_tex_hpr(TextureStage.get_default(), -180)
+
+        # box.hprInterval(20, (720, 0, 0)).start()
+        self.box = box
+
+    def mouse_click(self):
+        self.dragging = True
+        self.dragging_start_time = globalClock.get_frame_time()
+
+    def mouse_release(self):
+        self.dragging = False
+        self.before_mouse_pos = None
+
+    def rotate_camera(self, mouse_pos, dt):
+        if self.before_mouse_pos:
+            angle = Vec3()
+
+            if (delta := mouse_pos.x - self.before_mouse_pos.x) < 0:
+                angle.x += 180
+            elif delta > 0:
+                angle.x -= 180
+
+            if (delta := mouse_pos.y - self.before_mouse_pos.y) < 0:
+                angle.z -= 180
+            elif delta > 0:
+                angle.z += 180
+
+            angle *= dt
+            self.camera_root.set_hpr(self.camera_root.get_hpr() + angle)
+
+        self.before_mouse_pos = Vec2(mouse_pos.xy)
+
+    def update(self, task):
+        dt = globalClock.get_dt()
+
+        if self.mouseWatcherNode.has_mouse():
+            mouse_pos = self.mouseWatcherNode.get_mouse()
+
+            if self.dragging:
+                if globalClock.get_frame_time() - self.dragging_start_time >= 0.2:
+                    self.rotate_camera(mouse_pos, dt)
+
+        return task.cont
+
+
+def test():
+    inx = np.linspace(0, 10, 20, endpoint=True)
+    iny = np.linspace(0, 10, 20, endpoint=True)
+    inz = np.linspace(0, 10, 20, endpoint=True)
+    mx, my, mz = np.meshgrid(inx, iny, inz)
+
+    arr = []
+    for ax in range(len(mx)):
+        for ay in range(len(my)):
+            for az in range(len(mz)):
+                arr += [[mx[ax][ay][az], my[ax][ay][az], mz[ax][ay][az]]]
+
+    points = np.array(arr)
+    vor = Voronoi(points)
+
+    for idx, vts in enumerate(vor.vertices):
+        yield vts
+        # print(f'vertices {idx} {vts}')
+
+
+def output(arr, stem, parent='.', with_suffix=True):
+    if with_suffix:
+        now = datetime.now()
+        stem = f'{stem}_{now.strftime("%Y%m%d%H%M%S")}'
+
+    file_path = f'{parent}/{stem}.png'
+    # file_name = f'{stem}_{now.strftime("%Y%m%d%H%M%S")}.{ext}'
+    cv2.imwrite(file_path, arr)
+
+
+
+
+
+if __name__ == '__main__':
+    app = VoronoiCube()
+    app.run()
